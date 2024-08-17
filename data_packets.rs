6602d6967f87b42ab08d63522d2e45f96c1a1677
NickyNocerino <>
2024-08-16 23:20:49 -0400

    finished packet 256

--- data_packets.rs
+++ data_packets.rs
@@ -5,10 +5,10 @@ pub struct Packet64 {
 }
 
 pub struct Packet256 {
-    data0: u64,
-    data1: u64,
-    data2: u64,
-    data3: u64,
+    data0: Packet64,
+    data1: Packet64,
+    data2: Packet64,
+    data3: Packet64,
     occupancy: i32
 }
 
@@ -25,5 +25,49 @@ impl Packet64 {
 impl Packet256 {
     pub fn new() -> Packet256 {
         Packet256{0,0,0,0,0}
+    }
+    pub fn add(self, new:Packet64) -> Nil {
+        match self.occupancy {
+            0 => {
+                self.d0 = new;
+                self.occupancy = 1;
+            }
+            1 => {
+                self.d1 = new;
+                self.occupancy = 2;
+            }
+            2 => {
+                self.d2 = new;
+                self.occupancy = 3;
+            }
+            3 =>{
+                self.d3 = new;
+                self.occupancy = 4;
+            }
+            4 => Nil
+        }
+    }
+    pub fn pop(self) -> Nil {
+        match self.occupancy {
+            0 => {
+                Nil
+            }
+            1 => {
+                self.occupancy = 0;
+                self.d0
+            }
+            2 => {
+                self.occupancy = 1;
+                self.d1;
+            }
+            3 =>{
+                self.occupancy = 2;
+                self.d2
+            }
+            4 => {
+                self.occupancy = 3
+                self.d3
+            }
+        }
     }
 }
